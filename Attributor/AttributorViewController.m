5927498d6348035acf435795132f1781ba1cf6ae
Kangya <liaokangya@hotmail.com>
2013-11-06 11:20:52 +0800

    Lesson 5

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -11,9 +11,51 @@
 @interface AttributorViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *body;
 @property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 @end
 
 @implementation AttributorViewController
+
+- (void)viewDidLoad // only gets called once before you go on screen
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeWidthAttributeName : @3,
+                           NSStrokeColorAttributeName : self.outlineButton.tintColor }
+                   range:NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification *)notification
+{
+    [self usePreferredFonts];
+}
+
+- (void)usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
 
 - (IBAction)changeBodySelectionColourToMatchBackgroundOfButton:(UIButton *)sender
 {
@@ -35,10 +77,6 @@
                                      range:self.body.selectedRange];
 }
 
-- (void)viewDidLoad // only gets called once before you go on screen
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+
 
 @end
